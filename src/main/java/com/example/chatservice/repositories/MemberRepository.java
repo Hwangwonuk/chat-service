5b8185404eafe1e22f4b7cb8c3cdf7cbbd58cb92
UK <hok0173@naver.com>
2025-11-21 19:29:10 +0900

    feat: MemberRepository

---
+++ src/main/java/com/example/chatservice/repositories/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.chatservice.repositories;
+
+import com.example.chatservice.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+}
