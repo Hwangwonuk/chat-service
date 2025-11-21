15c2d9774c2af64f88397719d25188bd4a871712
UK <hok0173@naver.com>
2025-11-21 17:53:17 +0900

    feat: Member Entity 설계

---
+++ src/main/java/com/example/chatservice/entities/Member.java
@@ -0,0 +1,30 @@
+package com.example.chatservice.entities;
+
+import com.example.chatservice.enums.Gender;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String email;
+
+    String nickName;
+
+    String name;
+
+    String phoneNumber;
+
+    LocalDate birthDay;
+
+    @Enumerated
+    Gender gender;
+
+    String role;
+
+}
---
+++ src/main/java/com/example/chatservice/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.example.chatservice.enums;
+
+public enum Gender {
+
+    MALE, FEMALE
+
+}
