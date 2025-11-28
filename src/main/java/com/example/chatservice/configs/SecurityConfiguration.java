4398459e622307f6e43e9a8d84454b873e61c27b
UK <hok0173@naver.com>
2025-11-28 17:04:58 +0900

    feat: OAuth2 Google 추가

--- src/main/java/com/example/chatservice/configs/SecurityConfiguration.java
+++ src/main/java/com/example/chatservice/configs/SecurityConfiguration.java
@@ -19,6 +19,7 @@ public class SecurityConfiguration {
         httpSecurity
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
 
         return httpSecurity.build();
@@ -28,7 +29,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain consultantSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .securityMatcher("/consultants/**", "login")
+                .securityMatcher("/consultants/**")
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(HttpMethod.POST, "/consultants").permitAll()
                         .anyRequest().hasRole("CONSULTANT"))
